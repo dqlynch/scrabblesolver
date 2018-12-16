aaa8291f99b1c3cffed1e7497b83d7b4dddf4ea2
Dylan Lynch <dqlynch@umich.edu>
2018-12-16 14:20:52 -0500

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+        long_description = fh.read()
+
+setuptools.setup(
+    name="scrabble_solver",
+    version="0.0.1",
+    author="Dylan Lynch",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/dqlynch/scrabblesolver",
+    packages=setuptools.find_packages(),
+    install_requires=[
+      'numpy',
+      'DAWG',
+      'DAWG-Python',
+    ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
